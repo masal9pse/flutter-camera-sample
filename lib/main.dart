15f8d4a013ff09fa5ba7d64332eccbe85f4c3d43
masal9pse <returnymgstokh@icloud.com>
2021-07-02 18:37:07 +0900

    camera_cameraプラグインを使用して、動作

--- lib/main.dart
+++ lib/main.dart
@@ -1,145 +1,77 @@
-// @dart=2.9
 import 'dart:io';
-
-// import 'package:camera/camera.dart';
-// import 'package:camera/camera.dart';
-import 'package:flutter/src/foundation/diagnostics.dart';
-import 'package:flutter_better_camera/camera.dart';
+// import "package:camera_camera/camera_camera.dart";
+import 'package:camera_camera/camera_camera.dart';
+// import 'package:flutter_better_camera/camera.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_camera_testing/next_page.dart';
-import 'package:flutter_camera_testing/zoom_able.dart';
-import 'package:path/path.dart';
-import 'package:path_provider/path_provider.dart';
-import 'after_take_picture_camera_view.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/services.dart'; // 追加
 
-Future<void> main() async {
-  // runAppが実行される前に、cameraプラグインを初期化
-  WidgetsFlutterBinding.ensureInitialized();
-
-// デバイスで使用可能なカメラの一覧を取得する
-  final cameras = await availableCameras();
-
-  print(cameras);
-// 利用可能なカメラの一覧から、指定のカメラを取得する
-  final firstCamera = cameras[0];
-  // runApp(MyApp());
-  // SystemChrome.setPreferredOrientations([
-  //   // DeviceOrientation.landscapeLeft,
-  //   DeviceOrientation.landscapeRight,
-  // ]).then((_) {
-    // runApp(new MyApp());
-    runApp(CameraHome(camera: firstCamera));
-  // });
-  // runApp(CameraHome(camera: firstCamera));
+void main() {
+  runApp(MyApp());
 }
 
-class CameraHome extends StatefulWidget {
-  final CameraDescription camera;
-
-  // CameraHome({required this.camera});
-  // const CameraHome({Key? key, required this.camera}) : super(key: key);
-  const CameraHome({this.camera});
-
+class MyApp extends StatelessWidget {
   @override
-  State<StatefulWidget> createState() => CameraHomeState();
-}
-
-class CameraHomeState extends State<CameraHome> {
-  // デバイスのカメラを制御するコントローラ
-  CameraController _cameraController;
-
-  // コントローラーに設定されたカメラを初期化する関数
-  Future<void> _initializeCameraController;
-
-  @override
-  void initState() {
-    super.initState();
-
-    // SystemChrome.setPreferredOrientations([
-    //   DeviceOrientation.landscapeRight,
-    //   // DeviceOrientation.portraitUp,
-    //   // DeviceOrientation.portraitDown,
-    // ]);
-    // ②
-    // コントローラを初期化
-    _cameraController = CameraController(
-        // 使用するカメラをコントローラに設定
-        widget.camera,
-        ResolutionPreset.max);
-    // ③
-    // コントローラーに設定されたカメラを初期化
-    _initializeCameraController = _cameraController.initialize();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Camera Camera 2.0',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Camera Camera 2.0'),
+    );
   }
+}
 
-  // ④
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
   @override
-  void dispose() {
-    // ウィジェットが破棄されたタイミングで、カメラのコントローラを破棄する
-    _cameraController.dispose();
-    super.dispose();
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final photos = <File>[];
+
+  void openCamera() {
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (_) => CameraCamera(
+              onFile: (file) {
+                photos.add(file);
+                Navigator.pop(context);
+                setState(() {});
+              },
+            )));
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('カメラ画面'),
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: GridView.builder(
+        gridDelegate:
+        SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+        itemCount: photos.length,
+        itemBuilder: (_, index) => Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+            width: size.width,
+            child: Image.file(
+              photos[index],
+              fit: BoxFit.cover,
+            ),
+          ),
         ),
-        // FutureBuilderを実装
-        body: FutureBuilder<void>(
-          future: _initializeCameraController,
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.done) {
-              // カメラの初期化が完了したら、プレビューを表示
-              // return CameraPreview(_cameraController);
-              return ZoomableWidget(
-                  child: CameraPreview(_cameraController),
-                  onTapUp: (scaledPoint) {
-                    // controller.setPointOfInterest(scaledPoint);
-                  },
-                  onZoom: (zoom) {
-                    print('zoom');
-                    if (zoom < 11) {
-                      _cameraController.zoom(zoom);
-                    }
-                  });
-            } else {
-              // カメラの初期化中はインジケーターを表示
-              return const Center(child: CircularProgressIndicator());
-            }
-          },
-        ),
-        floatingActionButton: FloatingActionButton(
-          child: const Icon(Icons.camera_alt),
-          // ボタンが押下された際の処理
-          onPressed: () async {
-            try {
-              // 画像を保存するパスを作成する
-              final path = join(
-                (await getApplicationDocumentsDirectory()).path,
-                '${DateTime.now()}.png',
-              );
-              print(12345);
-              print(path);
-              // カメラで画像を撮影する
-              await _cameraController.takePicture(path);
-
-              // 画像を表示する画面に遷移
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => CameraView(imgPath: path),
-                ),
-              );
-            } catch (e) {
-              print(e);
-            }
-          },
-        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: openCamera,
+        child: Icon(Icons.camera_alt),
       ),
     );
   }
