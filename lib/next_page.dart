7fb5ce6c7f3f059511a9418a32413c5d40e63f9c
masal9pse <returnymgstokh@icloud.com>
2021-07-10 00:51:53 +0900

    一時保存

--- lib/next_page.dart
+++ lib/next_page.dart
@@ -1,19 +1,45 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class NextPage extends StatelessWidget{
+class MyHomePage extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text('Picture'),
-        ),
-        body: Container(
-          height: double.infinity,
-          color: Colors.red,
-        )
-    );
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    // 2.呼び出されると状態が更新されて画面に反映される
+    setState(() {
+      _counter++;
+    });
   }
 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('test'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter, // 1.ボタンを押したら関数が呼ばれる
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
 }
