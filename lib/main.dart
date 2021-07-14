7754f9ba1476db34e74caa3b48d4973ba23b8566
masal9pse <returnymgstokh@icloud.com>
2021-07-14 14:44:15 +0900

    画像の回転にサンプルコードで成功

--- lib/main.dart
+++ lib/main.dart
@@ -1,68 +1,99 @@
 // @dart=2.9
-import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:flutter_image_compress/flutter_image_compress.dart';
-import 'package:image_picker/image_picker.dart';
-import 'package:path_provider/path_provider.dart' as path_provider;
+import 'package:flutter/services.dart'; // rootBundle
+import 'package:image/image.dart' as imgLib; // <- Dart Image Library
+import 'dart:typed_data'; // Uint8List
 
-// import 'package:ar_war/cam.dart';
+void main() => runApp(MyApp());
 
-Future<void> main() async {
-  // SystemChrome.setEnabledSystemUIOverlays([]);
-  runApp(MaterialApp(
-    home: Cam(),
-  ));
-}
-
-class Cam extends StatefulWidget {
+// MyApp ウィジェットクラス
+class MyApp extends StatelessWidget {
   @override
-  _Cam createState() => _Cam();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.light(),
+      home: MyHomePage(title: 'Dart Image Library'),
+    );
+  }
 }
 
-class _Cam extends State<Cam> {
-  final ImagePicker _picker = ImagePicker();
-  String imageText = "";
-  String statusText = "";
-  File localImageFile;
+// MyHomePage ウィジェットクラス
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+// MyHomePage ステートクラス
+class _MyHomePageState extends State<MyHomePage> {
+  imgLib.Image _image;
+  List<int> _imageBytes;
+
+  // アセットから画像を読み込む関数
+  void loadAssetImage() async {
+    ByteData imageData = await rootBundle.load('images/test.png');
+    _image = imgLib.decodeImage(Uint8List.view(imageData.buffer));
+    // imgLib.copyRotate(_image, 90);
+    _imageBytes = imgLib.encodeJpg(_image);
+    setState((){});
+  }
+
+  // 初期化
+  @override
+  void initState() {
+    // 初期ダミー画像作成
+    _image = imgLib.Image(250, 250);
+    _imageBytes = imgLib.encodeJpg(_image);
+
+    // アセットから画像を読み込む
+    loadAssetImage();
+
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('タイトルです'),
+        title: Text(widget.title),
       ),
       body: Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text(
-            statusText,
-            style: TextStyle(fontSize: 16),
-          ),
-          Container(
-            height: 100,
-            child: localImageFile != null
-                ? Image.file(localImageFile)
-                : Text(imageText),
-          ),
-          RaisedButton(
-              child: Text("Choose Image"),
-              onPressed: () async {
-                try {
-                  final pickedFile =
-                      await _picker.getImage(source: ImageSource.gallery);
-                  setState(() {
-                    localImageFile = File(pickedFile.path);
-                    statusText = "Image Chosen";
-                  });
-                } catch (e) {
-                  setState(() {
-                    imageText = "Error selecting image";
-                  });
-                }
-              }),
-        ],
-      )),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('オリジナル画像'),
+            SizedBox(width: 250, height:  100, child: Image.asset('images/test.png')),
+
+            SizedBox(height: 30),
+
+            Text('フィルター画像'),
+            SizedBox(width: 250, height: 250, child: Image.memory(_imageBytes)),
+
+            RaisedButton(
+              child: Text('フィルター'),
+              onPressed: () {
+                // var image = _image.clone();
+                // // 画像のクローンを作成
+                // imgLib.Image cloneImage = image.clone();
+
+// リサイズされたコピーを作成
+//                 var image = imgLib.copyResize(_image, width: 200);
+                var image = imgLib.copyRotate(_image,180);
+                // ----- フィルター処理テスト
+                imgLib.vignette(image);
+                imgLib.gaussianBlur(image, 5);
+                imgLib.grayscale(image);
+                imgLib.copyRotate(image, 120);
+                // imgLib.copyResize(image,width: 120);
+                // -----
+                _imageBytes = imgLib.encodeJpg(image);
+                setState((){});
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
